705f0750df4a1fb8e1c3286e0ea915dfd22300f4
Matheus Claudino <matheusclaudin@gmail.com>
2014-11-21 03:58:03 -0200

    metodo listar estampa adicionado

--- src/dao/daoAtributos.java
+++ src/dao/daoAtributos.java
@@ -9,6 +9,7 @@ import java.sql.SQLException;
 import java.util.List;
 import model.Categoria;
 import model.Cor;
+import model.Estampa;
 import model.Fornecedor;
 import model.Tamanho;
 import org.hibernate.HibernateException;
@@ -89,6 +90,26 @@ public class daoAtributos {
             sessao.beginTransaction();
 
             List<Cor> resultado = sessao.createCriteria(Cor.class).list();
+
+            sessao.getTransaction().commit();
+            sessao.close();
+            return resultado;
+        } catch (HibernateException he) {
+            if (sessao != null) {
+                sessao.getTransaction().rollback();
+                sessao.close();
+            }
+            System.out.println("ERRO listar CIDADE: " + he.getMessage());
+            return null;
+        }
+    }
+      public List<Estampa> listarEstampa() {
+        Session sessao = null;
+        try {
+            sessao = util.HibernateUtil.getSessionFactory().openSession();
+            sessao.beginTransaction();
+
+            List<Estampa> resultado = sessao.createCriteria(Estampa.class).list();
 
             sessao.getTransaction().commit();
             sessao.close();
