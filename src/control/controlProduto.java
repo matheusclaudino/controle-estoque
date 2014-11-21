b71d26db25584d653582f05e8a34594f88afe650
Matheus Claudino <matheusclaudin@gmail.com>
2014-11-21 03:15:44 -0200

    adicionado os metodos alterar e inserir adicionados ao controlador

--- src/control/controlProduto.java
+++ src/control/controlProduto.java
@@ -7,7 +7,10 @@ package control;
 
 import dao.daoProduto;
 import java.math.BigDecimal;
+import java.sql.SQLException;
 import java.util.Date;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 import model.Categoria;
 import model.Cor;
 import model.Estampa;
@@ -33,7 +36,27 @@ public class controlProduto {
         pro = new Produto(codigo, categoria, cor, estampa, fornecedor, tamanho, nome, descricao, unidade, preco, dataEntrada);
         
         dao.insert(pro);
-        return 0;
+        return pro.getCodigo();
+    }
+    public void alterarProduto(int codigo,String nome, String descricao, BigDecimal preco, int unidade, Date dataEntrada, Object forne,
+                Object cate, Object tam, Object c, Object est){
+        Produto pro;
+        Fornecedor fornecedor = (Fornecedor) forne;
+        Categoria categoria = (Categoria) cate;
+        Tamanho tamanho = (Tamanho) tam;
+        Cor cor = (Cor) c;
+        Estampa estampa = (Estampa) est;
+        pro = new Produto(codigo, categoria, cor, estampa, fornecedor, tamanho, nome, descricao, unidade, preco, dataEntrada);
+        
+        dao.update(pro);
+    }
+    public void excluirPessoa(Produto produto, JTable tabela) throws SQLException, Exception {
+
+        dao.delete(produto);
+
+        // Remover a linha selecionado
+        int linha = tabela.getSelectedRow();
+        ((DefaultTableModel) tabela.getModel()).removeRow(linha);
     }
            
 }
