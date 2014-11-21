5eda8de682652109642ed9b02b216e0a66741e47
Matheus Claudino <matheusclaudin@gmail.com>
2014-11-21 03:42:16 -0200

    metodo carregarComboFornecedor adicionado no controlador de atributos

--- src/control/controlAtributos.java
+++ src/control/controlAtributos.java
@@ -5,10 +5,28 @@
  */
 package control;
 
+import dao.daoAtributos;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
+
 /**
  *
  * @author Matheus Claudino
  */
 public class controlAtributos {
-    
+    daoAtributos dao;
+    public void carregarComboFornecedor(JComboBox Fornecedor) {
+        List lista;
+        try {
+            lista = dao.listarFornecedor();
+            Fornecedor.setModel(new DefaultComboBoxModel(lista.toArray()));
+        } catch (Exception ex) {
+            Fornecedor.setModel(null);
+            Logger.getLogger(controlAtributos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
 }
