11acc91de24c5dad6f17500c531ce683eec1c492
Matheus Claudino <matheusclaudin@gmail.com>
2014-11-21 01:21:02 -0200

    assinatura dos metodos alteradas, para instanciar o objeto do tipo Endereco, dentro da controladora, e criado um metodo que verifica o tipo do sexo

--- src/control/controlPessoa.java
+++ src/control/controlPessoa.java
@@ -31,34 +31,40 @@ public class controlPessoa {
         lista = null;
     }
 
-    public int inserirPessoa(Endereco endereco, String nome, String telefone, String cpf,
-            char sexo, Date dataNascimento, double salario, Date dataAdmissao, String cnpj, int tipo)//os valores especificos podem ser null
+    public int inserirPessoa(String nome, String telefone, String cep, String cidade, String rua, int numero, String bairro,
+                String complemento, String referencia, String estado, int tipoPessoa, String cpf, Date dataNascimento,
+                String sexo, String cnpj, Double salario, Date dataAdmissao)//os valores especificos podem ser null
             throws SQLException, Exception {
-       
+        
+        char s = tipoSexo(sexo);
         Pessoa pes = null;
-        if (tipo == 'C') {
-            pes = new Pessoafisica(endereco, nome, telefone, cpf, sexo, dataNascimento);
-        } else if (tipo == 'F') {
+        Endereco endereco = new Endereco(cep, rua, numero, bairro, cidade, estado);
+        if (tipoPessoa == 'C') {
+            pes = new Pessoafisica(endereco, nome, telefone, cpf, s, dataNascimento);
+        } else if (tipoPessoa == 'F') {
             pes = new Fornecedor(endereco, nome, telefone, cnpj);
-        } else if (tipo == 'V') {
-            pes = new Vendedor(endereco, nome, telefone, cpf, sexo, dataNascimento, salario, dataAdmissao);
+        } else if (tipoPessoa == 'V') {
+            pes = new Vendedor(endereco, nome, telefone, cpf, s, dataNascimento, salario, dataAdmissao);
         }
         dao.inserir(pes);
         return pes.getIdPessoa();
 
     }
 
-    public void alterarPessoa(int idPessoa, Endereco endereco, String nome, String telefone, String cpf,
-            char sexo, Date dataNascimento, double salario, Date dataAdmissao, String cnpj, int tipo)//os valores especificos podem ser null
+    public void alterarPessoa(int idPessoa, String nome, String telefone, String cep, String cidade, String rua, int numero, String bairro,
+                String complemento, String referencia, String estado, int tipoPessoa, String cpf, Date dataNascimento,
+                String sexo, String cnpj, Double salario, Date dataAdmissao)//os valores especificos podem ser null
             throws SQLException, Exception {
         
+        char s = tipoSexo(sexo);
         Pessoa pes = null;
-        if (tipo == 'C') {
-            pes = new Pessoafisica(endereco, nome, telefone, cpf, sexo, dataNascimento);
-        } else if (tipo == 'F') {
+        Endereco endereco = new Endereco(cep, rua, numero, bairro, cidade, estado);
+        if (tipoPessoa == 'C') {
+            pes = new Pessoafisica(endereco, nome, telefone, cpf, s, dataNascimento);
+        } else if (tipoPessoa == 'F') {
             pes = new Fornecedor(endereco, nome, telefone, cnpj);
-        } else if (tipo == 'V') {
-            pes = new Vendedor(endereco, nome, telefone, cpf, sexo, dataNascimento, salario, dataAdmissao);
+        } else if (tipoPessoa == 'V') {
+            pes = new Vendedor(endereco, nome, telefone, cpf, s, dataNascimento, salario, dataAdmissao);
         }
 
         pes.setIdPessoa(idPessoa);
@@ -82,5 +88,15 @@ public class controlPessoa {
 
         mostrarLista(tabela, tipo);
 
+    }
+    
+    public char tipoSexo(String sexo){
+        char s;
+        if(sexo.equalsIgnoreCase("Feminino"))
+           s = 'F';
+        else
+            s = 'M';
+        
+        return s;
     }
 }
