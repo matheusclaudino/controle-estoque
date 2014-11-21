d1119714dbefe050e1512b461e69ede77153dfcd
Matheus Claudino <matheusclaudin@gmail.com>
2014-11-21 03:48:19 -0200

    metodo listarCategoria adicionado

--- src/dao/daoAtributos.java
+++ src/dao/daoAtributos.java
@@ -7,6 +7,7 @@ package dao;
 
 import java.sql.SQLException;
 import java.util.List;
+import model.Categoria;
 import model.Fornecedor;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
@@ -16,24 +17,46 @@ import org.hibernate.Session;
  * @author Matheus Claudino
  */
 public class daoAtributos {
-     public List<Fornecedor> listarFornecedor() throws SQLException, Exception {
 
-       Session sessao = null;
+    public List<Fornecedor> listarFornecedor() throws SQLException, Exception {
+
+        Session sessao = null;
         try {
             sessao = util.HibernateUtil.getSessionFactory().openSession();
             sessao.beginTransaction();
-                                  
+
             List<Fornecedor> resultado = sessao.createCriteria(Fornecedor.class).list();
-         
-            sessao.getTransaction().commit();  
+
+            sessao.getTransaction().commit();
             sessao.close();
             return resultado;
         } catch (HibernateException he) {
-            if ( sessao != null ) {
+            if (sessao != null) {
                 sessao.getTransaction().rollback();
                 sessao.close();
             }
-            System.out.println("ERRO listar CIDADE: " + he.getMessage() );
+            System.out.println("ERRO listar CIDADE: " + he.getMessage());
+            return null;
+        }
+    }
+
+    public List<Categoria> listarCategoria() {
+        Session sessao = null;
+        try {
+            sessao = util.HibernateUtil.getSessionFactory().openSession();
+            sessao.beginTransaction();
+
+            List<Categoria> resultado = sessao.createCriteria(Categoria.class).list();
+
+            sessao.getTransaction().commit();
+            sessao.close();
+            return resultado;
+        } catch (HibernateException he) {
+            if (sessao != null) {
+                sessao.getTransaction().rollback();
+                sessao.close();
+            }
+            System.out.println("ERRO listar CIDADE: " + he.getMessage());
             return null;
         }
     }
