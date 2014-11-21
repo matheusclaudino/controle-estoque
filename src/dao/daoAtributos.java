7779fc17ab206acea5ea377f94b7850fcf915e76
Matheus Claudino <matheusclaudin@gmail.com>
2014-11-21 04:42:19 -0200

    metodos insert dos atributos adicionados

--- src/dao/daoAtributos.java
+++ src/dao/daoAtributos.java
@@ -14,13 +14,83 @@ import model.Fornecedor;
 import model.Tamanho;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import util.HibernateUtil;
 
 /**
  *
  * @author Matheus Claudino
  */
 public class daoAtributos {
+    
+    public void insertCor(Cor cor){
+        Session sessao = null;
+        try {
+            sessao = HibernateUtil.getSessionFactory().openSession();
+            sessao.beginTransaction();
 
+            sessao.save(cor);
+
+            sessao.getTransaction().commit();
+        } catch (HibernateException he) {
+            sessao.getTransaction().rollback();
+        } finally {
+            if (sessao != null) {
+                sessao.close();
+            }
+        }
+    }
+    public void insertEstampa(Estampa estampa){
+        Session sessao = null;
+        try {
+            sessao = HibernateUtil.getSessionFactory().openSession();
+            sessao.beginTransaction();
+
+            sessao.save(estampa);
+
+            sessao.getTransaction().commit();
+        } catch (HibernateException he) {
+            sessao.getTransaction().rollback();
+        } finally {
+            if (sessao != null) {
+                sessao.close();
+            }
+        }
+    }
+    public void insertCategoria(Categoria categoria){
+        Session sessao = null;
+        try {
+            sessao = HibernateUtil.getSessionFactory().openSession();
+            sessao.beginTransaction();
+
+            sessao.save(categoria);
+
+            sessao.getTransaction().commit();
+        } catch (HibernateException he) {
+            sessao.getTransaction().rollback();
+        } finally {
+            if (sessao != null) {
+                sessao.close();
+            }
+        }
+    }
+    public void insertTamanho(Tamanho tamanho){
+        Session sessao = null;
+        try {
+            sessao = HibernateUtil.getSessionFactory().openSession();
+            sessao.beginTransaction();
+
+            sessao.save(tamanho);
+
+            sessao.getTransaction().commit();
+        } catch (HibernateException he) {
+            sessao.getTransaction().rollback();
+        } finally {
+            if (sessao != null) {
+                sessao.close();
+            }
+        }
+    }
+    
     public List<Fornecedor> listarFornecedor() throws SQLException, Exception {
 
         Session sessao = null;
