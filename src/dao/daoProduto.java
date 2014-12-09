88bddd92dd36cb5cabed7866a4c661761ddce84a
Matheus Claudino <matheusclaudin@gmail.com>
2014-12-09 15:57:44 -0200

    codigos comentados removidos.

--- src/dao/daoProduto.java
+++ src/dao/daoProduto.java
@@ -78,18 +78,12 @@ public class daoProduto {
 
     public List getProduto(int id) {
         Session sessao = null;
-//        Produto produto = null;
+
         try {
             sessao = dao.HibernateUtil.getSessionFactory().openSession();
             sessao.beginTransaction();
-
-            // GET: se não existir, retorna NULL
-            // LOAD: se não existir, retorna ERRO (Exception)
-//            if(id == -1){
-//                 produto = (Produto) sessao.get(Produto.class);
-//            }else{
-                Produto produto = (Produto) sessao.get(Produto.class, new Integer(id));
-//            }
+ 
+            Produto produto = (Produto) sessao.get(Produto.class, new Integer(id));
             
             Criteria cons = sessao.createCriteria(Produto.class);
             cons.setFetchMode("fornecedor", FetchMode.JOIN);
@@ -100,7 +94,7 @@ public class daoProduto {
             
             cons.add(Restrictions.like("codigo", id));
             cons.addOrder(Order.asc("codigo"));
-
+            
             List<Produto> res = cons.list();
             
             sessao.getTransaction().commit();
